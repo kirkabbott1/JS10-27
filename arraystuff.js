020d896a4c825fde2bcd34886390c1d97476f31d
Kirk Abbott <kirkabbott1@gmail.com>
2016-10-27 14:48:46 -0400

    updates exercises/readme

---
+++ arraystuff.js
@@ -0,0 +1,24 @@
+// var numbs = [-1, 3, 5, -4];
+//
+// function pos(n) {
+//   return n > 0;
+// }
+// var turnpos = numbs.filter(pos);
+// console.log(turnpos);
+
+// var moreNumbs = [2,3,4,5];
+//
+// function even(n) {
+//   return n % 2 === 0;
+// }
+// var turneven = moreNumbs.filter(even);
+// console.log(turneven)
+//
+// function square(n) {
+//   n * n;
+}
+var turnsquare = function(n) {
+  n * n;
+};
+moreNumbs.square;
+n.forEach(turnsquare);
--- objectDict.js
+++ objectDict.js
@@ -1,32 +1,32 @@
-var phonebookDict = {
-  Alice: '703-493-1834',
-  Bob: '857-384-1234',
-  Elizabeth: '484-584-2923'
-}
-
-
-
-// for (var things in phonebookDict) {
-//   var definition = phonebookDict[things];
-//   console.log(things + ': ' + definition);
+// var phonebookDict = {
+//   Alice: '703-493-1834',
+//   Bob: '857-384-1234',
+//   Elizabeth: '484-584-2923'
 // }
-
-phonebookDict['kirk'] = "404-219-2572"
-
+//
+//
+//
+// // for (var things in phonebookDict) {
+// //   var definition = phonebookDict[things];
+// //   console.log(things + ': ' + definition);
+// // }
+//
+// phonebookDict['kirk'] = "404-219-2572"
+//
+// // console.log(phonebookDict);
+//
+// delete phonebookDict['Alice']
+//
+// // console.log(phonebookDict);
+//
+// phonebookDict['Bob'] = "999-999-9991"
+//
 // console.log(phonebookDict);
-
-delete phonebookDict['Alice']
-
-// console.log(phonebookDict);
-
-phonebookDict['Bob'] = "999-999-9991"
-
-console.log(phonebookDict);
 
 function histogram(input) {
   var counts = {};
   for (var i = 0; i < input.length; i++) {
-    var char = input(i);
+    var char = input[i];
     if (!counts[char]) {
       counts[char] = 1;
     } else {
